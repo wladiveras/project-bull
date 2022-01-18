7669db0f67b45e2d4f8048ca267298d808cab6e3
wladiveras <wladinart@gmail.com>
2022-01-18 16:30:27 -0300

    :adhesive_bandage: FakerMock fix

--- backend/src/DataFixtures/AppFixtures.php
+++ backend/src/DataFixtures/AppFixtures.php
@@ -25,11 +25,13 @@ class AppFixtures extends Fixture
             $manager->persist($customer);
         }
 
-        for ($i = 0; $i < 1500; $i++) {
+        for ($i = 0; $i < 5500; $i++) {
             $bull = new Bull();
             $bull->setCode(abs(crc32(uniqid())));
             $bull->setWeight(mt_rand(200, 1000));
-            $bull->setBirthday($faker->dateTimeBetween($startDate = '-10 years', $endDate = 'now'));
+            $bull->setBirthday(
+                mt_rand(1, 12) . "-" . mt_rand(1, 28) . "-" . mt_rand(2000, 2021) . " " . mt_rand(0, 23) . ":" . mt_rand(0, 59) . ":" . mt_rand(0, 59)
+            );
             $bull->setWeekMilk(mt_rand(0, 100));
             $bull->setWeekFood(mt_rand(0, 60));
             $bull->setStatus($status[mt_rand(0, 1)]);
